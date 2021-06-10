355c49c233cabcbfab2840c1585d80ce65a042ad
Rifdi Ardhi <rifdi.ardhi@klikdaily.com>
2021-06-10 18:35:27 +0700

    Local Search

--- lib/local_search.dart
+++ lib/local_search.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LocalSearch extends StatefulWidget {
   const LocalSearch({Key key}) : super(key: key);
@@ -8,8 +11,53 @@ class LocalSearch extends StatefulWidget {
 }
 
 class _LocalSearchState extends State<LocalSearch> {
+  List data = [];
+
+  Future<void> loadJsonData() async {
+    var jsonText = await rootBundle.loadString('assets/file/category.json');
+    setState(() => data = json.decode(jsonText));
+  }
+
+  TextEditingController editingController = TextEditingController();
+
+  @override
+  void initState() {
+    loadJsonData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(hintText: 'Search here...'),
+            onChanged: (value) {
+              setState(() {});
+            },
+            controller: editingController,
+          ),
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: data.length,
+              itemBuilder: (ctx, index) {
+                if (editingController.text.isEmpty) {
+                  return ListTile(
+                    title: Text('${data[index]['title']}'),
+                  );
+                } else if (data[index]['title']
+                    .toLowerCase()
+                    .contains(editingController.text)) {
+                  return ListTile(
+                    title: Text('${data[index]['title']}'),
+                  );
+                } else {
+                  return Container();
+                }
+              })
+        ],
+      ),
+    );
   }
 }
